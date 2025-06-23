65e6267c41ef703cb61fe15b7fd6c01fac24bfc9
Preiss, Sandy <apreiss@rti.org>
2025-06-23 13:30:44 -0400

    Remove files from experiment template that arent used in this notebook-based experiment

--- crcsim/experiment/deploy_to_aws.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker buildx build --platform linux/amd64 -t crcsim . &&
-
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 547883312134.dkr.ecr.us-east-2.amazonaws.com &&
-
-docker tag crcsim:latest 547883312134.dkr.ecr.us-east-2.amazonaws.com/crcsim:latest &&
-
-docker push 547883312134.dkr.ecr.us-east-2.amazonaws.com/crcsim:latest
