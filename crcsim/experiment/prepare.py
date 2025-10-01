85bd46524c348bb18905038d50e28ebfa40eca4a
korourke5 <kate.orourke@implenomics.com>
2025-10-01 11:40:16 -0400

    Update files to include new NC scenario

--- crcsim/experiment/prepare.py
+++ crcsim/experiment/prepare.py
@@ -1,6 +1,5 @@
 import json
 import random
-from copy import deepcopy
 from enum import Enum, unique
 from pathlib import Path
 from typing import Callable, Dict, List, Optional
@@ -201,121 +200,56 @@ def create_scenarios() -> List:
     # For each health center, define the initial compliance rate in the baseline
     # scenario and the implementation scenario and vary diagnostic compliance.
     initial_compliance = {
-        "fqhc1": (0.522, 0.593),
-        "fqhc2": (0.154, 0.421),
-        "fqhc3": (0.519, 0.568),
-        "fqhc4": (0.278, 0.374),
-        "fqhc5": (0.383, 0.572),
-        "fqhc6": (0.211, 0.392),
-        "fqhc7": (0.257, 0.354),
-        "fqhc8": (0.190, 0.390),
+        "NC": (0.097, 0.300),
     }
-    low_initial_stage_3_treatment_cost = 67_300
-    low_initial_stage_4_treatment_cost = 97_931
-    extra_low_initial_stage_3_treatment_cost = 50_000
-    extra_low_initial_stage_4_treatment_cost = 80_000
 
     diagnostic_compliance_rates = {
-        "100Compliance": 1.0,
-        "80Compliance": 0.8,
+        "NC": (0.444, 0.688),
     }
 
     FIT_cost_dict = {
         "Public": 22,
-        "Patient+Public": 44,
+        # "Patient-Public": 44,
     }
 
     Colonoscopy_cost_dict = {
         "Public": 912,
-        "Patient+Public": 1437,
+        # "Patient-Public": 1437,
     }
 
     scenarios = []
 
-    for fqhc, screening_rates in initial_compliance.items():
+    for site, screening_rates in initial_compliance.items():
         for compliance, diagnostic_rate in diagnostic_compliance_rates.items():
-            for cost_category in (
-                FIT_cost_dict.keys() & Colonoscopy_cost_dict.keys()
-            ):  # Only common keys
+            for cost_category in FIT_cost_dict.keys() & Colonoscopy_cost_dict.keys():
+                IRR = 1.19
                 FIT_cost = FIT_cost_dict[cost_category]
                 Col_cost = Colonoscopy_cost_dict[cost_category]
                 baseline = (
                     Scenario(
-                        name=f"{fqhc}_{cost_category}_{compliance}_baseline",
+                        name=f"{site}_{cost_category}_{compliance}_baseline",
                         params=get_default_params(),
                     )
                     .transform(transform_initial_compliance(screening_rates[0]))
-                    .transform(transform_diagnostic_compliance(diagnostic_rate))
+                    .transform(transform_diagnostic_compliance(diagnostic_rate[0]))
                     .transform(transform_test_cost(Test.FIT, FIT_cost))
                     .transform(transform_test_cost(Test.COLONOSCOPY, Col_cost))
+                    .transform(transform_lesion_risk_alpha(IRR))
                 )
                 scenarios.append(baseline)
 
                 implementation = (
                     Scenario(
-                        name=f"{fqhc}_{cost_category}_{compliance}_implementation",
+                        name=f"{site}_{cost_category}_{compliance}_implementation",
                         params=get_default_params(),
                     )
                     .transform(transform_initial_compliance(screening_rates[1]))
-                    .transform(transform_diagnostic_compliance(diagnostic_rate))
+                    .transform(transform_diagnostic_compliance(diagnostic_rate[1]))
                     .transform(transform_test_cost(Test.FIT, FIT_cost))
                     .transform(transform_test_cost(Test.COLONOSCOPY, Col_cost))
+                    .transform(transform_lesion_risk_alpha(IRR))
                 )
                 scenarios.append(implementation)
-
-                # Sensitivity analysis 2. Lower cost for stage III and stage IV initial phase
-                baseline_low_cost = deepcopy(baseline)
-                baseline_low_cost.transform(
-                    transform_treatment_cost(
-                        "3", "initial", low_initial_stage_3_treatment_cost
-                    )
-                ).transform(
-                    transform_treatment_cost(
-                        "4", "initial", low_initial_stage_4_treatment_cost
-                    )
-                )
-                baseline_low_cost.name = f"{fqhc}_{cost_category}_{compliance}_baseline_low_initial_treat_cost"
-                scenarios.append(baseline_low_cost)
-
-                implementation_low_cost = deepcopy(implementation)
-                implementation_low_cost.transform(
-                    transform_treatment_cost(
-                        "3", "initial", low_initial_stage_3_treatment_cost
-                    )
-                ).transform(
-                    transform_treatment_cost(
-                        "4", "initial", low_initial_stage_4_treatment_cost
-                    )
-                )
-                implementation_low_cost.name = f"{fqhc}_{cost_category}_{compliance}_implementation_low_initial_treat_cost"
-                scenarios.append(implementation_low_cost)
-
-                # Sensitivity analysis 2a. Extra low cost for stage III and stage IV initial phase
-                baseline_extra_low_cost = deepcopy(baseline)
-                baseline_extra_low_cost.transform(
-                    transform_treatment_cost(
-                        "3", "initial", extra_low_initial_stage_3_treatment_cost
-                    )
-                ).transform(
-                    transform_treatment_cost(
-                        "4", "initial", extra_low_initial_stage_4_treatment_cost
-                    )
-                )
-                baseline_extra_low_cost.name = f"{fqhc}_{cost_category}_{compliance}_baseline_extra_low_initial_treat_cost"
-                scenarios.append(baseline_extra_low_cost)
-
-                implementation_extra_low_cost = deepcopy(implementation)
-                implementation_extra_low_cost.transform(
-                    transform_treatment_cost(
-                        "3", "initial", extra_low_initial_stage_3_treatment_cost
-                    )
-                ).transform(
-                    transform_treatment_cost(
-                        "4", "initial", extra_low_initial_stage_4_treatment_cost
-                    )
-                )
-                implementation_extra_low_cost.name = f"{fqhc}_{cost_category}_{compliance}_implementation_extra_low_initial_treat_cost"
-                scenarios.append(implementation_extra_low_cost)
 
     # No screening baseline scenario
     no_screening = (
--- crcsim/experiment/run_iteration.sh
+++ crcsim/experiment/run_iteration.sh
@@ -12,7 +12,7 @@ if [ ! -d "$output_dir" ]; then
   mkdir $output_dir
 fi
 
-aws s3 cp "s3://crcsim-exp-fqhc-diagnostic-compliance-comp-additional-scenarios/scenarios/$scenario/params.json" "./params.json" 
+aws s3 cp "s3://crcsim-exp-accsis-scenarios/scenarios/$scenario/params.json" "./params.json" 
 
 crc-simulate \
     --npeople=$npeople \
@@ -23,5 +23,5 @@ crc-simulate \
 crc-analyze \
     --params-file=./params.json &&
 
-aws s3 cp ./results.csv "s3://crcsim-exp-fqhc-diagnostic-compliance-comp-additional-scenarios/scenarios/$scenario/results_$iteration.csv"
-aws s3 cp ./output.csv "s3://crcsim-exp-fqhc-diagnostic-compliance-comp-additional-scenarios/$scenario/output_$iteration.csv"
+aws s3 cp ./results.csv "s3://crcsim-exp-accsis-scenarios/scenarios/$scenario/results_$iteration.csv"
+aws s3 cp ./output.csv "s3://crcsim-exp-accsis-scenarios/$scenario/output_$iteration.csv"
